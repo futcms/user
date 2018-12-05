848ee9ca2627a47453a25479fb76e2b3983513ad
futcms <34991938+futcms@users.noreply.github.com>
2018-12-06 02:02:47 +0700

    Create user.php

---
+++ user.php
@@ -0,0 +1,9 @@
+<?php
+$msgtoto = "[!] tidak di ijinkan"."\n"."[!] menjual skrip ini kepada siapapun"."\n"."[!] msg by Mafut Ragil";
+$msgyd = "[!] enjoy your life";
+$msgwe = "[!] enjoy your life";
+
+$data= [
+'Mafut Ragil', ];
+$ydnews = ['tes',];
+$toto = ['tes',];
